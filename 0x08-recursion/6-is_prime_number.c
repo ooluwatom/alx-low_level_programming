a5d6a7c16c90afbe95af2ca87c7693e710efef3e
ooluwatom <ooluwatom@gmail.com>
2022-07-21 16:04:12 +0100

    Final Local Commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,12 +11,12 @@ int is_prime_number(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-	
+
 	if (n % i == 0 && i > 1)
 		return (0);
 
 	if ((n / i) < 1)
 		return (1);
-	
+
 	return (is_prime_number(n, i + 1));
 }
